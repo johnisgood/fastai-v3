a0d2aa198c8f0fcea8e957d243066d7d6c9a82b2
Simon AZAR <simon.azar@free.fr>
2019-03-20 16:13:03 +0100

    change to my colab model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://colab.research.google.com/drive/16iSGdXo7p4P4y4LxzCB5GMWzSGGO7-JG'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['0', '1', '2','3','4','5','6','7','8','9']
 path = Path(__file__).parent
 
 app = Starlette()
